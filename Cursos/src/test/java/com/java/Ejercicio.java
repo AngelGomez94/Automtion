f3ba4808d31d7f738ac1e59294810ac82bd45269
AngelGomez94 <angelgomez140994@gmail.com>
2021-01-30 12:31:57 -0600

    QA

---
+++ Cursos/src/test/java/com/java/Ejercicio.java
@@ -0,0 +1,34 @@
+package com.java;
+
+public class Ejercicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		Escribe un metodo que se llame hasEqualSum con 3 parametros de tipo int.
+//
+//		El metodo debe regresar un valor booleano verdadero si la suma de los dos primeros parametros es igual a la suma del tercer parametro, en otro caso devolver false.
+//
+//		Aqui algunos ejemplos de input y output:
+//
+//		* hasEqualSum(1, 1, 1); deberia regresar FALSE porque la suma de 1+1 no es 1
+//
+//		* hasEqualSum(1, 1, 2); deberia regresar TRUE porque la suma de 1+1 si es 2
+		
+		boolean isEqualsum= hasequalsum(1,2,3);
+		System.out.println("El valor es: "+isEqualsum);
+		
+	}
+	public static boolean hasequalsum(int parametro1, int parametro2, int parametro3) {
+		
+			
+		if (parametro1+parametro2 == parametro3) {
+			
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	
+}
